da2646ad8bb30582e0e4a812a4a84a2aaf13eb5a
harsh-env <mpddriller@live.com>
2022-04-06 14:08:30 -0500

    added output file in text and printing results in terminal

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,21 +1,19 @@
-# from audioop import avg
-# from operator import index
-from calendar import month
+
 import os
 import csv
-import math
-# from sqlite3 import Row
-# from statistics import mean
+
+
 csvpath = os.path.join("Resources", "budget_data.csv")
+output_file = os.path.join("analysis", "financial_analysis.txt")
 with open(csvpath, encoding='utf-8') as csvfile: 
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
-    print(f"CSV Header : {csv_header}")
+    
     total_months =0
     total = 0
     cur_val=[]
     prev_val= []
-    # next_val=0
+   
     diff1=0
     diff2=0
     Max_Val=0
@@ -32,37 +30,37 @@ with open(csvpath, encoding='utf-8') as csvfile:
         else:
             prev_val = int(prev_row[1])
         Diff = (int(cur_val) - int(prev_val))
-        # diff.append(int(cur_val) - int(prev_val))
-        # max_val = max(diff)
+       
         diff_list.append(Diff)
-          # if int(diff) > int(diff2): 
-        #     print(diff, total_months)
-        #     Max_Val = diff
-        # else:
-        #     Max_Val = diff2
-        
         diff1 = diff1+ Diff
         if total_months < 2: 
             Avg_change =0
         else: 
-            # Avg_change = diff1/(total_months-1)
-            Avg_change = diff1/(len(diff_list)-1)
-        total += int((row[1]))
+            Avg_change = round(diff1/(len(diff_list)-1), 2)
+            total += int((row[1]))
         prev_row = row
         diff2 = Diff
     max_val = max(diff_list)
     max_val_month = months[diff_list.index(max_val)]
     min_val = min(diff_list)
     min_val_month = months[diff_list.index(min_val)]
-    print("Total Months:", total_months)
-    print("Total:", total)
-    print("total change:", Avg_change)
-    print("Greatest Increase in Profits:", max_val_month, max_val)
-    print("Greatest Dncrease in Profits:", min_val_month, min_val)
-    # print(diff_list)
-    # print(diff[total_months])
+       
+    with open(output_file, "w") as txt_file:
+     financial_analysis = (f"""
+     Financial Analysis
+     -------------------------
+        Total Months: {total_months}
+        Total: ${total}
+        Average Change: {Avg_change}
+        Greatest Increase in Profits: {max_val_month} {max_val}
+        Greatest Decrease in Profits: {min_val_month} {min_val}
+     -------------------------         
+                             """)
+    print(financial_analysis)
+    
+    with open(output_file, "w") as txt_file:
+        for lines in financial_analysis:
+            txt_file.write(lines)
 
-    # for i in range
-  # else:
-            # diff[total_months]= int(row[1])-int(prev_row[1])
-        # diff[total_months]= prev_row[1]  
+   
+     

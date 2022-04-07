5ba6c2a3376d8c2f5cd38e369764cbb697a4aacc
harsh-env <mpddriller@live.com>
2022-04-07 00:14:44 -0500

    checked the code - it is calculating everything we want it to calculate. But I am struggling with how to pull the numbers out of the dictionary for results.

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -8,6 +8,8 @@ output_file = os.path.join("analysis", "election_analysis.txt")
 Total_Votes=0
 can_names = []
 can_votes = {} 
+selected_votes=[]
+selected_vote_percent=[]
 # dict()
 Winning_Candidate = ""
 Winning_Count =0
@@ -20,7 +22,7 @@ with open(csvpath, encoding='utf-8') as csvfile:
     for row in csvreader:
         Total_Votes += 1
         can_n = row[2]
-        if can_n != can_names:
+        if can_n != can_nm1:
         # if can_n != can_nm1:
             can_names.append(can_n)
             # can_number +=1
@@ -28,23 +30,26 @@ with open(csvpath, encoding='utf-8') as csvfile:
             can_votes[can_n] += 1
         else:
             can_votes[can_n] += 1
-            # Candidate_Votes[Candidate_Name] = 0
-            # Candidate_Votes[Candidate_Name] += 1
+            
         can_nm1 = can_n
         
         for candidate in can_votes:
             votes = can_votes.get(candidate)
-            # can_votes[candidate].append(votes)
-            #add key for votes in can_votes
             vote_percent = round((float(votes)/float(Total_Votes))*100, 3)
+            
+            # can_votes[candidate] = votes
             # if votes > Winning_Count: 
             #     Winning_Count = votes
-            #     Winning_Candidate = Candidate_Name
-    print(can_votes, vote_percent, "%")
-    # print(can_votes, votes)
+            #     Winning_Candidate = can_n
+    # print(can_votes, vote_percent, "%")
+    # print(can_votes)
     # , vote_percent, "%")
+# print(can_votes.keys, can_names[1])
+# print(can_votes.items())
+# print(can_names[1], selected_votes[1], selected_vote_percent[1])
 
 print(Total_Votes)
+# print({votes})
 
 # print(votes)
 # print(vote_percent)
@@ -52,17 +57,17 @@ print(Total_Votes)
 
 # with open(output_file, "w") as txt_file:
 #      election_results = (f"""
-# #     Election Results
-# #     -------------------------
+#     Election Results
+#     -------------------------
 #     Total Votes: {Total_Votes}
-# #     -------------------------  
-# #     {Candidate_Name}: {Vote_percent} {votes}
-# #     {Candidate_Name}: {Vote_percent} {votes}
-# #     {Candidate_Name}: {Vote_percent} {votes}
-# #     ------------------------- 
-# #     Winner : {Winning_Candidate}          
-# #                     """)
-# #     print(election_results)
+#     -------------------------  
+#     {can_n}: {vote_percent} {votes}
+#     {can_n}: {vote_percent} {votes}
+#     {can_n}: {vote_percent} {votes}
+#     ------------------------- 
+#     # Winner : {Winning_Candidate}          
+#                     """)
+# print(election_results)
 
 
     
